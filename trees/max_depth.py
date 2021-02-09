56d10c27e2b5c3a8252435530e123e2296e6088f
Evaline <evalinebai@gmail.com>
2021-02-08 16:02:34 -0800

    Add trees/max_depth

---
+++ trees/max_depth.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class SolutionIterative:
+    def iterativeMaxDepth(self, root: TreeNode) -> int:
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.right))
+                stack.append((current_depth + 1, root.left))
+        return depth
+
+    def recursiveMaxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left = self.maxDepth(root.left)
+            right = self.maxDepth(root.right)
+            return max(left, right) + 1
